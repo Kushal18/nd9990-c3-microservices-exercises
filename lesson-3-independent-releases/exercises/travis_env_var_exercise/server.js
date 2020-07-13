d8200e377229390f44f3b644939e436a2cb9a74e
“Kushal <kjoshi@marketo.com>
2020-07-12 17:59:25 -0700

    adding an env variable

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -6,6 +6,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`Testing travis ${process.env.TRAVIS_TESTING}`)
     await sleep(5000);
   }
 }
